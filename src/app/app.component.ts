82e18bda83da3bbb74f23176b808d9a40cb36b26
Alejandro Campos Palacios <alcampospalacios@gmail.com>
2020-10-13 14:16:34 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/scrolling';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements AfterViewInit{
   title = 'ScrollingDistpachProblem';
+
+  constructor(private scrollDispatcher: ScrollDispatcher) {}
+
+  ngAfterViewInit(): void {
+    this.scrollDispatcher.scrolled().subscribe((event: CdkScrollable) => {      
+      console.log(event);
+    });
+  }
 }
+
